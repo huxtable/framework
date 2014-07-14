1affc4ce189978e771d3d38b78e5245ea53002ac
Ashur Cabrera <hello@cabreramade.co>
2014-07-14 09:52:22 -0700

    added Output

---
+++ src/Huxtable/Output.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of Huxtable
+ */
+namespace Huxtable;
+
+class Output
+{
+	/**
+	 * @param	string	$string			String to colorize
+	 * @param	string	$foreground		Name of foreground color
+	 * @param	string	$background		Name of background color
+	 * @return	string
+	 */
+	public static function colorize($string, $foreground=null, $background=null)
+	{
+		$foregroundColors =
+		[
+			'black'		=> '0;30',
+			'red'		=> '0;31',
+			'green'		=> '0;32',
+			'yellow'	=> '0;33',
+			'blue'		=> '0;34',
+			'purple'	=> '0;35',
+			'cyan'		=> '0;36',
+			'gray'		=> '0;37',
+		];
+
+		$backgroundColors =
+		[
+			'black'		=> '40',
+			'red'		=> '41',
+			'green'		=> '42',
+			'yellow'	=> '43',
+			'blue'		=> '44',
+			'purple'	=> '45',
+			'cyan'		=> '46',
+			'gray'		=> '47',
+		];
+
+		$colorized = '';
+		$colorized .= isset($foregroundColors[$foreground]) ? "\033[".$foregroundColors[$foreground]."m" : '';
+		$colorized .= isset($backgroundColors[$background]) ? "\033[".$backgroundColors[$background]."m" : '';
+		$colorized .= $string;
+
+		return $colorized;
+	}
+}
+
+?>
