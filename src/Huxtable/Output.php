f4b03388487849a3965a630e78df6a9ce0032698
Ashur Cabrera <hello@cabreramade.co>
2014-07-14 20:44:06 -0700

    added block formatting to Output

--- src/Huxtable/Output.php
+++ src/Huxtable/Output.php
@@ -8,6 +8,56 @@ namespace Huxtable;
 class Output
 {
 	/**
+	 * Returns string in formatted, padded block
+	 *
+	 * @param	string	$string			String to colorize
+	 * @param	string	$foreground		Name of foreground color
+	 * @param	string	$background		Name of background color
+	 * @param	string	$padding		Block padding
+	 * @param	string	$indent			Block indent
+	 * @return	string
+	 */
+	public static function block($string, $foreground=null, $background=null, $padding=2, $indent=0)
+	{
+		$length  = strlen($string) + (2 * $padding);
+		$empty   = '';
+		$content = '';
+
+		for($i=1; $i<=$length; $i++)
+		{
+			$empty .= ' ';
+		}
+
+		for($j=1; $j<= (2 * $padding) + 1; $j++)
+		{
+			if($j == $padding + 1)
+			{
+				$content .= $string;
+			}
+			else
+			{
+				$content .= ' ';
+			}
+		}
+
+		$result[] = self::colorize($empty, $foreground, $background);
+		$result[] = self::colorize($content, $foreground, $background);
+		$result[] = self::colorize($empty, $foreground, $background);
+
+		for($k=0; $k<count($result); $k++)
+		{
+			$left = '';
+			for($l=0; $l<$indent; $l++)
+			{
+				$left .= ' ';
+			}
+
+			$result[$k] = $left . $result[$k];
+		}
+
+		return implode(PHP_EOL, $result).PHP_EOL;
+	}
+	/**
 	 * @param	string	$string			String to colorize
 	 * @param	string	$foreground		Name of foreground color
 	 * @param	string	$background		Name of background color
