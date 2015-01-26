b64e953c22e517fba4a30e2bdd767bf6524da03f
Ashur Cabrera <hello@cabreramade.co>
2015-01-26 11:38:57 -0800

    added Output buffering

--- src/Huxtable/Output.php
+++ src/Huxtable/Output.php
@@ -8,6 +8,11 @@ namespace Huxtable;
 class Output
 {
 	/**
+	 * @var string
+	 */
+	protected $buffer='';
+
+	/**
 	 * @param	string	$string			String to colorize
 	 * @param	string	$foreground		Name of foreground color
 	 * @param	string	$background		Name of background color
@@ -46,6 +51,30 @@ class Output
 		$colorized .= "\033[0m";
 
 		return $colorized;
+	}
+
+	/**
+	 * @return	string
+	 */
+	public function flush()
+	{
+		return $this->buffer;
+	}
+
+	/**
+	 * @param	string	$string
+	 */
+	public function line($string)
+	{
+		$this->buffer = $this->buffer . $string . PHP_EOL;
+	}
+
+	/**
+	 * @return	void
+	 */
+	public function unshiftLine($string)
+	{
+		$this->buffer = $string . PHP_EOL . $this->buffer;
 	}
 }
 
