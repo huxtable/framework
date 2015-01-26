94bec23b0427f27775c52039f09851a3d8ab5d3b
Ashur Cabrera <hello@cabreramade.co>
2015-01-26 11:42:25 -0800

    added Format library

---
+++ src/Huxtable/Format.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of Huxtable
+ */
+namespace Huxtable;
+
+class Format
+{
+	/**
+	 * @param	string	$timestamp
+	 * @return	string	Date string formatted like `ls` dates
+	 */
+	public static function date($timestamp=null)
+	{
+		if(is_null($timestamp))
+		{
+			$timestamp = time();
+		}
+
+		$now  = getdate();
+		$date = getdate($timestamp);
+
+		$detail = ($now[0] - $date[0] <= 15778500) ? sprintf('%2s:%02s', $date['hours'], $date['minutes']) : $date['year'];
+		
+		return sprintf('%.3s %2s %5s', $date['month'], $date['mday'], $detail);
+	}
+}
+
+?>
