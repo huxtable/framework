6152bda200bbae004ef7a1b181fb25bac6fa22b6
Ashur Cabrera <hello@cabreramade.co>
2015-08-09 16:08:07 -0700

    added support for upgrading

--- src/Huxtable/Application.php
+++ src/Huxtable/Application.php
@@ -8,10 +8,18 @@ namespace Huxtable;
 use Huxtable\Command\CommandInvokedException;
 use Huxtable\Command\IncorrectUsageException;
 use Huxtable\FileInfo;
+use Huxtable\Git;
+use Huxtable\HTTP;
 use Huxtable\InvalidCommandException;
 
 class Application
 {
+	const UPGRADE_ALREADY_NEWEST  = 10;
+	const UPGRADE_ERR_PHP_VERSION = 20;
+	const UPGRADE_ERR_PULL        = 30;
+	const UPGRADE_ERR_SUBMODULE   = 40;
+	const UPGRADE_SUCCESSFUL      = 50;
+
 	/**
 	 * @var array
 	 */
@@ -90,8 +98,24 @@ class Application
 		// Register default commands
 		$help = new Command('help', "Display help information about {$this->name}", [$this, 'commandHelp']);
 		$help->setUsage('help <command>');
-
 		$this->registerCommand($help);
+
+		if( isset( $this->projectPath ) && isset( $this->remoteConfigURL ) )
+		{
+			$upgrade = new \Huxtable\Command( 'upgrade', "Fetch the newest version of {$this->name}", [$this, 'commandUpgrade'] );
+			$upgrade->registerOption( 'verbose' );
+			$upgradeUsage = <<<USAGE
+upgrade [options]
+
+OPTIONS
+     --verbose
+         be verbose
+
+
+USAGE;
+			$upgrade->setUsage( $upgradeUsage );
+			$this->registerCommand($upgrade);
+		}
 	}
 
 	/**
@@ -183,6 +207,52 @@ OUTPUT;
 		$output = sprintf($output, trim($usage), $descriptions);
 
 		return $output.PHP_EOL;
+	}
+
+	/**
+	 * Expose self::upgrade as a command-line command
+	 */
+	protected function commandUpgrade()
+	{
+		$repo = new Git\Repository( $this->projectPath );
+
+		try
+		{
+			$commandOptions = $this->input->getCommandOptions();
+			$verbose = isset( $commandOptions['verbose'] );
+			$upgrade = $this->upgrade( new HTTP, $repo, $verbose );
+		}
+		catch( \Exception $e )
+		{
+			$this->logError( $e->getMessage() );
+			throw new CommandInvokedException( 'An error occurred while retrieving upgrade information. Please try again later.', 1 );
+		}
+
+		switch( $upgrade['status'] )
+		{
+			case self::UPGRADE_ALREADY_NEWEST:
+				return "{$this->name} {$this->version} is currently the newest version available.";
+				break;
+
+			case self::UPGRADE_SUCCESSFUL:
+				return "Successfully upgraded to {$this->name} {$upgrade['remoteVersion']}";
+				break;
+
+			case self::UPGRADE_ERR_PHP_VERSION:
+				$phpLocalVersion = phpversion();
+				return "{$this->name} {$upgrade['remoteVersion']} requires PHP v{$upgrade['phpMinimumVersion']}, found v{$phpLocalVersion}";
+				break;
+
+			case self::UPGRADE_ERR_PULL:
+				$this->logError( "Git Error: An error occurred during 'git pull'. Run '{$this->name} upgrade --verbose'" );
+				throw new CommandInvokedException( 'An error occurred while upgrading. Please try again later.', 1 );
+				break;
+
+			case self::UPGRADE_ERR_SUBMODULE:
+				$this->logError( "Git Error: An error occurred during 'git submodule update'. Run '{$this->name} upgrade --verbose'" );
+				throw new CommandInvokedException( 'An error occurred while upgrading. Please try again later.', 1 );
+				break;
+		}
 	}
 
 	/**
@@ -397,6 +467,130 @@ OUTPUT;
 		{
 			unset($this->commands[$name]);
 		}
+	}
+
+	/**
+	 * Compare local and remote versions, fetch update if necessary
+	 *
+	 * @param	Huxtable\HTTP			$http
+	 * @param	Huxtable\Git\Repository	$repo
+	 * @param	boolean					$verbose
+	 * @return	array
+	 */
+	public function upgrade( HTTP $http, Git\Repository $repo, $verbose=false )
+	{
+		if( $verbose )
+		{
+			echo 'Checking remote version... ';
+		}
+		$response = $http->get( $this->remoteConfigURL );
+
+		$error = $response->getError();
+		if( $error['code'] != 0 )
+		{
+			if( $verbose )
+			{
+				echo 'failed.' . PHP_EOL;
+			}
+
+			throw new \Exception( "curl Error: {$error['message']}", $error['code'] );
+		}
+
+		$status = $response->getStatus();
+		if( $status['code'] != 200 )
+		{
+			if( $verbose )
+			{
+				echo 'failed.' . PHP_EOL;
+			}
+			throw new \Exception( "HTTP Error: {$status['message']} ({$this->remoteConfigURL})", $status['code'] );
+		}
+
+		$body = $response->getBody();
+		$remoteConfig = json_decode( $body, true );
+		if( json_last_error() != JSON_ERROR_NONE )
+		{
+			if( $verbose )
+			{
+				echo 'failed.' . PHP_EOL;
+			}
+			throw new \Exception( 'JSON Error: ' . json_last_error_msg(), json_last_error() );
+		}
+
+		$result = [
+			'remoteVersion' => $remoteConfig['version'],
+			'phpMinimumVersion' => $remoteConfig['php-min']
+		];
+
+		// Should we update?
+		$remoteVersionIsNewer = version_compare( $this->version, $remoteConfig['version'], '<' );
+
+		if( $remoteVersionIsNewer )
+		{
+			if( $verbose )
+			{
+				echo 'update found.' . PHP_EOL;
+			}
+		}
+		else
+		{
+			if( $verbose )
+			{
+				echo 'done.' . PHP_EOL;
+			}
+			
+			$result['status'] = self::UPGRADE_ALREADY_NEWEST;
+			return $result;
+		}
+
+		$phpVersionMeetsRequirement = version_compare( phpversion(), $remoteConfig['php-min'], '>=' );
+
+		if( !$phpVersionMeetsRequirement )
+		{
+			$result['status'] = self::UPGRADE_ERR_PHP_VERSION;
+			return $result;
+		}
+
+		// Pull
+		if( $verbose )
+		{
+			echo 'Fetching update... ';
+		}
+		$resultPull = $repo->pull();
+
+		if( $resultPull['exitCode'] != 0 )
+		{
+			$result['status'] = self::UPGRADE_ERR_PULL;
+
+			if( $verbose )
+			{
+				echo 'failed.' . PHP_EOL;
+				echo $resultPull['output']['raw'] . PHP_EOL . PHP_EOL;
+			}
+			return $result;
+		}
+
+		$resultSubmodules = $repo->submoduleUpdate([
+			'init' => true,
+			'recursive' => true
+		]);
+
+		if( $resultSubmodules['exitCode'] != 0 )
+		{
+			$result['status'] = self::UPGRADE_ERR_SUBMODULE;
+
+			if( $verbose )
+			{
+				echo 'failed.' . PHP_EOL;
+				echo $resultSubmodules['output']['raw'] . PHP_EOL . PHP_EOL;
+			}
+			return $result;
+		}
+
+		echo 'done.' . PHP_EOL;
+
+		$result['status'] = self::UPGRADE_SUCCESSFUL;
+		return $result;
 	}
 }
 
